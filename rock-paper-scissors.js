fc68b74f0801187ed73873ee6a40caea51910291
dkim19375 <65050767+dkim19375@users.noreply.github.com>
2023-04-08 00:46:21 -0400

    Add JS

--- rock-paper-scissors.js
+++ rock-paper-scissors.js
@@ -1,3 +1,8 @@
+let points = 0;
+let rounds = 0;
+
+const moves = ["Rock", "Paper", "Scissors"];
+
 function getComputerChoice() {
     const random = Math.random() * 3;
     if (random < 1) return "Rock";
@@ -5,34 +10,60 @@ function getComputerChoice() {
     return "Scissors";
 }
 
-function playRound(playerSelection, computerSelection) {
-    if (playerSelection !== playerSelection.toLowerCase() || computerSelection !== computerSelection.toLowerCase()) {
-        return playRound(playerSelection.toLowerCase(), computerSelection.toLowerCase());
+function getResult(playerSelection, computerSelection) {
+    if (playerSelection === computerSelection) return "tie";
+    const playerI = moves.indexOf(playerSelection);
+    const computerI = moves.indexOf(computerSelection);
+    if (playerI + 1 === computerI || playerI - 2 === computerI) {
+        return "lost";
     }
-    if (playerSelection === computerSelection) {
-        return `Tie! You both picked ${playerSelection[0].toUpperCase()}${playerSelection.substring(1)}`
-    }
-    switch (playerSelection) {
-        case "rock": {
-            if (computerSelection === "paper") return "You Lose! Paper beats Rock";
-            return "You Win! Rock beats Scissors";
-        }
-        case "paper": {
-            if (computerSelection === "scissors") return "You Lose! Scissors beats Paper";
-            return "You Win! Paper beats Rock";
-        }
-        default: {
-            if (computerSelection === "rock") return "You Lose! Rock beats Scissors";
-            return "You Win! Scissors beats Paper";
-        }
-    }
+    return "won";
 }
 
-function game() {
-    console.log("Welcome to Rock Paper Scissors!\n");
-    for (let i = 1; i <= 5; i++) {
-        const playerSelection = prompt(`Round ${i}. Do you want to pick Rock, Paper, or Scissors?`);
-        console.log(playRound(playerSelection, getComputerChoice()));
-    }
+function playRound(playerSelection, computerSelection) {
+    document.getElementById('computer-selection-text').textContent = computerSelection;
+    const result = getResult(playerSelection, computerSelection);
+    setResult(result);
+}
 
+function setResult(won) {
+    let element = document.getElementById("result");
+    if (element == null) {
+        element = document.createElement("h2");
+        element.id = "result";
+        document.body.appendChild(element);
+    }
+    element.textContent = (won === "won") ? "You Won!" : (won === "tie" ? "Tie!" : "You Lost!")
+    if (won === "tie") return;
+    const pointCountElement = document.getElementById("point-count");
+    const winRateElement = document.getElementById("win-rate");
+    rounds++;
+    if (won === "won") {
+        points++;
+    }
+    const winRate = Math.round(points / rounds * 100);
+    pointCountElement.textContent = `Points: ${points}`;
+    winRateElement.textContent = `Win Rate: ${winRate}%`;
+}
+
+function onButtonClick() {
+    if (points === 5) return;
+    if (this.classList.contains("button-selected")) {
+        return;
+    }
+    resetButtons();
+    this.classList.add("button-selected");
+    playRound(this.textContent, getComputerChoice());
+    const currentRound = rounds;
+    setTimeout(() => {
+        if (currentRound !== rounds) return;
+        this.classList.remove("button-selected");
+    }, 200);
+}
+
+const buttons = Array.from(document.getElementsByClassName("player-selection"));
+buttons.forEach((b) => b.addEventListener('click', onButtonClick));
+
+function resetButtons() {
+    buttons.forEach((b) => b.classList.remove("button-selected"));
 }
